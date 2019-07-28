fac43324e89f2877631a5b49bfbf8d904d31a2a8
Pauline Vos <pvos88@gmail.com>
2019-07-28 17:46:23 +0200

    Load Tiled tilemap

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ var config = {
         preload: preload,
         create: create,
         update: update,
-    }
+    }   
 };
 
 var game = new Phaser.Game(config);
@@ -20,10 +20,17 @@ var floor;
 
 function preload() 
 {
+    this.load.image("tiles", "assets/images/tilemap.png");
+    this.load.tilemapTiledJSON("map", "assets/tilemap/tilemap.json");
 }
 
 function create()
 {
+    const map = this.make.tilemap({ key: "map" });
+
+    const tiles = map.addTilesetImage("tilemap", "tiles");
+    const backgroundLayer = map.createStaticLayer("belowPlayer", tiles, 0, 0);
+    const tvLayer = map.createStaticLayer("tvLayer", tiles, 0, 0);
 }
 
 function update()
